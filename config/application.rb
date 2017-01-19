ff83d88c60ebb6489befc404066bdba7c3f04796
Mary Elaine <marymahaffey1@gmail.com>
2017-01-19 17:13:13 -0500

    Add util.js file

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,11 @@ Bundler.require(*Rails.groups)
 
 module HikeShare
   class Application < Rails::Application
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-end
-
-config.to_prepare do
-  DeviseController.respond_to :html, :json
 end
